e473c36262fc1017ec5e98471635a85b117e2c9e
Dario Götz <dario.goetz@googlemail.com>
2020-09-15 22:28:13 +0200

    [mod] Draussen

--- app/roomcounter/core/config.py
+++ app/roomcounter/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     DB_URI: str = "sqlite:///./roomcounter.sqlite"
     INITIAL_USER: str = "dario"
     INITIAL_PASSWORD: str = "uiae"
-    INITIAL_ROOM_NAME: str = "Outside"
+    INITIAL_ROOM_NAME: str = "Draussen"
 
     SECRET_KEY: str = "c902190dc53dba745b1905dcec36aec84212eaab2992ce2eff0078e5bfde0082"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
